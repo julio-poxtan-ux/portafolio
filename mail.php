30968068fe31c648df7758de0738138e0d0edb4d
jucepoxtan-ui <jucepoxtan@gmail.com>
2017-07-25 04:26:36 -0500

    Add
    Agegados- Porta - Contacot- PHP

--- mail.php
+++ mail.php
@@ -3,12 +3,10 @@
     $email = $_POST['email'];
     $asunto = $_POST['asunto'];
     $message = $_POST['message'];
-    $formcontent="De: $name \n Asunto: $asunto \n Email: $email \n Mensaje: $message" ;
+    $formcontent="De: $name \nAsunto: $asunto \nEmail: $email \nMensaje: $message" ;
     $recipient = "jucepoxtan@gmail.com";
     $subject = "Formulario de contacto";
     $mailheader = "Correo: $email \r\n";
     mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-    // echo "Gracias por contactarte!" . " -" . "<a href='index.html' style='text-decoration:none; color:#ff0099; background: black; text-align: center;'> Regresar</a>";
     echo"<script type=\"text/javascript\">alert('El formulario fue enviado, me comunicare contigo :)'); window.location='index.html';</script>"; 
-    // echo "swal("Good job!", "You clicked the button!", "success")"; 
 ?>
